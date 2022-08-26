2af34b058b6b0c44744fc63787c501d0794ae760
Seregey <sergejm422@gmail.com>
2022-08-26 22:17:44 +0300

    get file detalis

--- PhP-master/src/public/upload.php
+++ PhP-master/src/public/upload.php
@@ -7,6 +7,14 @@ if (isset($_POST['import']) && $_POST['import'] == 'Import')
     if (isset($_FILES['filename']) && $_FILES['filename']['error'] === UPLOAD_ERR_OK)
     {
 
+        // Достаем детали файла
+        $file_tmp_path = $_FILES['filename']['tmp_name'];
+        $file_name = $_FILES['filename']['name'];
+        $file_size = $_FILES['filename']['size'];
+        $file_type = $_FILES['filename']['type'];
+        $file_name_perm = explode(".", $file_name);
+        $file_extension = strtolower(end($file_name_perm));
+
 
     }
 }
