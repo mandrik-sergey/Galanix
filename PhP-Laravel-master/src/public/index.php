a93a7e6e69235d17b364859753982b780fd9a151
Seregey <sergejm422@gmail.com>
2022-08-26 09:54:34 +0300

    first commit

---
+++ PhP-Laravel-master/src/public/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>Palmo Helloe world</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+<script src="script.js">  </script>
+<?php
+
+?>
+</body>
+</html>
