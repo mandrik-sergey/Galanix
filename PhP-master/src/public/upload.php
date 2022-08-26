1bd7983ca66ece4f95549467bae163b3178a6e45
Seregey <sergejm422@gmail.com>
2022-08-26 22:21:31 +0300

    upload file to directory

--- PhP-master/src/public/upload.php
+++ PhP-master/src/public/upload.php
@@ -15,8 +15,25 @@ if (isset($_POST['import']) && $_POST['import'] == 'Import')
         $file_name_perm = explode(".", $file_name);
         $file_extension = strtolower(end($file_name_perm));
 
+        // преобразовуем для более удобной проверки имя файла
+        $new_file_name = md5(time() . $file_name) . '.' . $file_extension;
 
+        // разрешение файлов которые доступны для загрузки
+        $file_extensions = array('csv');
+
+        if (in_array($file_extension, $file_extensions))
+        {
+            // загружаем файл в директорию
+            $upload_dir = './uploads/';
+            $dest_path = $upload_dir . $new_file_name;
+
+        }
+        else
+        {
+            $message = 'Upload failed. Allowed file types: ' . implode(',', $file_extensions);
+        }
     }
+
 }
 $_SESSION['message'] = $message;
 header("Location: index.php");
